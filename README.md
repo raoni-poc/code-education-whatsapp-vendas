ae49d37180d5e7afd696074cc733b1f643afc6ca
RaoniSporteman <raoni.sporteman@esales.com.br>
2019-04-23 20:00:20 -0300

    Concluído até a aula Recuperando Categorias

--- README.md
+++ README.md
@@ -25,7 +25,10 @@ PHPStorm - Plugins
   composer create-project --prefer-dist laravel/laravel:5.6.* code_shopping
   php artisan migrate:refresh
   php artisan app:name CodeShopping
-  
+  php artisan make:model Models\Category -m -f -r
+  php artisan make:seeder CategoriesSeeder
+  php artisan db:seed
+  php artisan route:list
  `
  
 ---------------
---
+++ code_shopping/app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace CodeShopping\Http\Controllers\Api;
+
+use CodeShopping\Http\Controllers\Controller;
+use CodeShopping\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \CodeShopping\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \CodeShopping\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \CodeShopping\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
